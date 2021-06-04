96e7e6acbcb3a548643019b215d1a16c7b5fe00b
vigan <vigan.abd@gmail.com>
2021-06-04 15:53:55 +0200

    fix: version checking

--- src/load.js
+++ src/load.js
@@ -9,7 +9,7 @@ const { decodeType } = require('./load.complex')
  */
 const load = (buff, ver = [4, 8]) => {
   const head = buff.slice(0, 2)
-  if (head[0] !== ver[0] && head[1] !== ver[1]) {
+  if (head[0] !== ver[0] || head[1] !== ver[1]) {
     throw new Error('ERR_MARSHAL_VERSION_NOT_SUPPORTED')
   }
 
